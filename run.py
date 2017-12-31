da56ab37f7d7c8aa1c7ba32834d263db8a5dd04b
DK <34984338+LeoDK@users.noreply.github.com>
2017-12-31 15:57:45 +0100

    Add files via upload

---
+++ run.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+from grid import Grid
+from time import sleep
+from os import system
+from random import random
+
+width = 30
+height = 30
+speed = 0.01
+
+colony = tuple( tuple( (1 if random() < 0.5 else 0) for _ in range(width) ) for _ in range(height) )
+
+g = Grid( (width,height), colony )
+
+try:
+	while True:
+		system('clear')
+		print "GENERATION {}".format(g.generation)
+		print g
+		sleep(speed)
+		g.evolve()
+
+except KeyboardInterrupt:
+	print "\nClosed"
